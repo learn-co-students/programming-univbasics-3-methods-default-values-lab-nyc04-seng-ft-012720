0574035995b9700297dff1b0ce80e42bc22b2c5d
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-10 19:26:45 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,16 @@
-# Your code here
-# For output purposes, use "puts" instead of "print" or "p"
+# # Your code here
+# # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+end 
+
+
+
+
+# Define a Return Value
+# The hosts are going the meal information to keep track of what everyone is eating, so make sure the return value includes the three food items you've chosen by returning: "A plate of #{protein} with #{veg1} and #{veg2}."
+
+# Note: An explicit or implicit return can be used.
